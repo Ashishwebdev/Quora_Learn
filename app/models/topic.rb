068a97f39e8672749fe6228f5e7e8ae970c34fe3
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-08-20 13:06:56 +0530

    data has been seeded in Topic table

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
   has_many :user_topics
   has_many :users, through: :user_topics
-  has_many :questions, dependent: destroy
+  has_many :questions, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+topic_list = ["Personality Development","Business","Spirituality", "Relations","Clothing","Pollution","Robotics","Web Technology","Fitness"]
+topic_list.each do |body|
+  Topic.create body: body
+end
+
