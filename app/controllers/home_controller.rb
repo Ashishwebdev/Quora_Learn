5af26199f284b0c582d74d95f9b892ca4922b5da
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-08-23 23:58:23 +0530

    Trying to get interested topics questions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
   def index
     @questions = Question.where(user_id: current_user.id)
     @topics = current_user.topics
+    @questions_interested = Array.new
+    @topics.each do |topic|
+      @questions_interested = Question.all.where(topic_id: topic.id)
+    end
   end
 end
