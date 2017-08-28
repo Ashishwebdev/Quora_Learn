07ce7955eb252b592e22db010c8df451ee5caec1
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-08-28 13:56:36 +0530

    Now comments can be created, updated and destroyed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,41 @@
 class CommentsController < ApplicationController
+
+  before_action :set_answer , only: [:create]
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  def create
+    comment =  @answer.comments.new(comment_params)
+    comment.save
+    @answer_comments = @answer.answer_comments.where(comment_id: comment.id).first_or_create
+    @user_comments = comment.user_comments.where(user_id: current_user).first_or_create
+  end
+
+  def edit
+
+  end
+
+  def update
+   @comment.update(comment_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:answer_id])
+  end
+
+ def comment_params
+    params.permit(:body)
+ end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   has_many :user_comments, dependent: :destroy
   has_many :users, through: :user_comments, dependent: :destroy
-  has_many :answer_comments
-  has_many :answers, through: :answer_comments
+  has_many :answer_comments, dependent: :destroy
+  has_many :answers, through: :answer_comments, dependent: :destroy
 end
